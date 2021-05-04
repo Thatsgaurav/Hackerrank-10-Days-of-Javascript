842cb8d9321a56dbce4d222db0ee137ff20d038b
Gaurav Singh <0201csai117@niet.co.in>
2021-05-04 03:03:11 +0100

    commit

--- Day8-Buttons-Container.js
+++ Day8-Buttons-Container.js
@@ -1,30 +1,11 @@
-class Rotator {
-    constructor(values) {
-        this.values = values;
-    }
-
-    rotation() {
-        this.values.unshift(this.values.pop());
-    }
-
-    round() {
-        this.rotation();
-        this.render();
-    }
-
-    render() {
-        const [btn1, btn2, btn3, btn6, btn9, btn8, btn7, btn4] = this.values;
-        const hashTable = {btn1, btn2, btn3, btn6, btn9, btn8, btn7, btn4};
-
-        for (const key in hashTable) {
-            document.getElementById(key).innerHTML = hashTable[key];
-        }
-    }
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    const rotator = new Rotator([1, 2, 3, 6, 9, 8, 7, 4]);
-    rotator.render();
-    document.getElementById('btn5').addEventListener('click', () => { rotator.round(); });
-
-});
+btn5.onclick = function () {
+  var viarableNum = btn1.innerHTML;
+  btn1.innerHTML = btn4.innerHTML;
+  btn4.innerHTML = btn7.innerHTML;
+  btn7.innerHTML = btn8.innerHTML;
+  btn8.innerHTML = btn9.innerHTML;
+  btn9.innerHTML = btn6.innerHTML;
+  btn6.innerHTML = btn3.innerHTML;
+  btn3.innerHTML = btn2.innerHTML;
+  btn2.innerHTML = viarableNum;
+};
